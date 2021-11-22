37292435f7909cf9c31e94bed2169140bea13fb3
mooenz <joss92821@hotmail.com>
2021-11-22 15:44:35 -0500

    arregla bug se para transicion

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ const carrusel = document.querySelector('.carrusel__items');
 
 let interval;
 let step = 1;
+const maxWidth = carrusel.scrollWidth - carrusel.clientWidth;
 
 const start = () => {
   interval = setInterval(() => {
-    const maxWidth = carrusel.scrollWidth - carrusel.clientWidth;
-
     carrusel.scrollLeft += step;
 
     if (carrusel.scrollLeft === maxWidth) {
       step = -1;
     } else if (carrusel.scrollLeft === 0) {
-      start();
-      step *= 0;
+      step = 1;
     }
   }, 10);
 };
 
-const stop = () => clearInterval(interval);
+const stop = () => {
+  clearInterval(interval);
+};
 
-//top cuando el cursor salga de la imagen
+
+//para cuando el cursor salga de la imagen
 carrusel.addEventListener('mouseover', stop, false);
 //inicia el carrusel
 carrusel.addEventListener('mouseout', start, false);
